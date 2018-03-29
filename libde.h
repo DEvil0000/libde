28647738eb54e350b4f0f329dad0d89a3f319b6f
Alexander Binzberger <alexander.binzberger@wingcon.com>
2018-03-29 10:55:07 +0200

    initial libde commit

---
+++ libde.h
@@ -0,0 +1,43 @@
+#define leer void
+#define buchstabe char
+#define ganzzahl int
+#define gleitkomma float
+#define kurz short
+#define lang long
+#define doppelt double
+#define automatisch auto
+#define vorzeichen signed
+#define positiv unsigned
+
+#define platz sizeof
+
+#define wenn if
+#define sonst else
+#define durchlaufe for
+#define solange while
+#define schalte switch
+#define fall case
+#define standard default
+#define mach do
+#define springe goto
+#define breche break
+#define weiter continue
+#define raus return
+
+#define versuche try
+#define wirf throw
+#define fang catch
+
+#define klasse class
+#define struktur struct
+#define auflistung enum
+#define einheit union
+#define definition typedef
+
+#define fremd extern
+#define feststehend static
+#define wechselnd volatile
+#define stetig const
+#define einzeiler inline
+#define einbuchen register
+
